ded12157047801679637cfae567c49dfa9eafa0e
nobru01 <brunoferreira01@gmail.com>
2023-08-15 21:18:02 -0300

    aula 1

---
+++ -robotron-2000/js/main.js
@@ -0,0 +1,9 @@
+const robotron=document.querySelector("#robotron")
+
+robotron.addEventListener("click",dizoi)
+
+function dizoi(evento){
+    // Manipula os dados completos do evento recebido
+    console.log(evento)
+    console.log("oi")
+}
